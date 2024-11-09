dc935675d96e34c4364942d586f9e54d78b0df57
Martinhak7655 <martinhakobyan954@gmail.com>
2024-11-09 20:00:14 +0400

    Last Updates

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import psycopg2
+import smtplib
+from email.mime.text import  MIMEText
+from email.mime.multipart import MIMEMultipart
 
 connection = psycopg2.connect(
     host="localhost",
@@ -11,6 +14,7 @@ create_table = '''
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(100) NOT NULL,
+        mail VARCHAR(100) NOT NULL,
         password VARCHAR(100) NOT NULL,
         create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
@@ -36,7 +40,6 @@ def quest(quest1=None, quest2=None):
         return True
     else:
         return False
-
 def admin(admin_username, admin_password):
     if admin_username == "" and admin_password == "":
         return False
@@ -49,13 +52,13 @@ def admin(admin_username, admin_password):
     print(f"Բառի Գալուստ ձեր էջ ադմին՝ {users[1]}")
     return True
 
-def addition(username_input,password_input):
-     if username_input == "" and password_input == "":
+def addition(username_input, password_input, mail_input):
+     if username_input == "" and password_input == "" and mail_input == "":
         return False
      insert = '''
-        INSERT INTO users (username, password) VALUES (%s, %s);
+        INSERT INTO users (username, password, mail) VALUES (%s, %s, %s);
      '''
-     cursor.execute(insert, (username_input, password_input,))
+     cursor.execute(insert, (username_input, password_input, mail_input,))
      connection.commit()
      insert2 = '''
          INSERT INTO tasks (password, task) VALUES (%s, %s);
@@ -64,15 +67,34 @@ def addition(username_input,password_input):
      connection.commit()
      return True
 
-def tasks(tasks_input, password1_input):
-    if tasks_input == "" and password1_input == "":
+def tasks(tasks_input, password1_input, mail_input2):
+    if tasks_input == "" and password1_input == "" and mail_input2 == "":
         return False
     update = '''
         UPDATE tasks SET task = (%s) WHERE password = (%s);
     '''
     cursor.execute(update, (tasks_input, password1_input,))
     connection.commit()
+    msg = MIMEMultipart()
+    msg["From"] = "martinhakobyan2024@mail.ru"
+    msg["To"] = mail_input2
+    msg["Subject"] = "Նոր Առաջադրանք"
+    msg.attach(MIMEText(f"{tasks_input}", 'plain'))
+    server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+
+    server.login("martinhakobyan2024@mail.ru", "0kjtQSdgJDtrFprzaNjf")
+    server.sendmail("martinhakobyan2024@mail.ru", mail_input2, msg.as_string())
     return True
+
+def info():
+    select = '''
+        SELECT * FROM users;
+    '''
+    cursor.execute(select)
+    connection.commit()
+    users = cursor.fetchall()
+    for x in users:
+        print(x)
 
 def delete_account(password_input, password4_input):
     delete = '''
@@ -99,13 +121,13 @@ def sign_in(password_input):
     print(users[1])
     return True
 
-def see_tasks(password_input3):
-    if password_input3 == "":
+def see_tasks(id_input3):
+    if id_input3 == "":
         return False
     select = '''
-        SELECT * FROM tasks WHERE password = (%s);
+        SELECT * FROM tasks WHERE id = (%s);
     '''
-    cursor.execute(select, (password_input3,))
+    cursor.execute(select, (id_input3,))
     connection.commit()
     task = cursor.fetchone()
     print(task[2])
@@ -124,13 +146,15 @@ while True:
                 print("Ավելացնել Աշղատակից։ A")
                 print("Փոփողել առաջադրանքները։  T")
                 print("Հեռացնել աշխատակցին։  D")
+                print("նայել Բոլոր տվյալները։  S")
                 print("-" * 25)
                 while True:
                     command = input("Ընտրեք Կամանդը։  ")
                     if command == "A":
                         add = input("Գրեք Աշխատակցի անունը։  ")
                         add2 = input("Գրեք Աշղատակցի գաղտնաբառը։  ")
-                        if addition(username_input=add, password_input=add2):
+                        add3 = input("Գրեք Աշխատակցու մաիլը")
+                        if addition(username_input=add, password_input=add2, mail_input=add3):
                             print("Տվյալները հաջողությամբ գրանցվեցին")
                         else:
                             print("-" * 25)
@@ -139,7 +163,8 @@ while True:
                     elif command == "T":
                         tasks_input2 = input("Գրեք Առաջադրանքը։  ")
                         password_input3 = input("Գրեք գաղտնաբառը ն թե ումնեք ուզում փոխել")
-                        if tasks(tasks_input=tasks_input2, password1_input=password_input3):
+                        mail_input3 = input("Գրեք աշխատակցու մաիլը")
+                        if tasks(tasks_input=tasks_input2, password1_input=password_input3, mail_input2=mail_input3):
                             print("Առաջադրանքները հաջողությամբ ուղարկվեցին")
                         else:
                             print("Գործողությունները դատարկ են !!")
@@ -152,6 +177,8 @@ while True:
                             print("-" * 25)
                             print("Գործողությունները դատարկ են !!")
                             print("-" * 25)
+                    elif command == "S":
+                        info()
                     else:
                         print("-" * 25)
                         print("Այդպիսի կոմանդ չկա !!")
@@ -170,8 +197,8 @@ while True:
             while True:
                 command = input("Ընտրեք կամանդը")
                 if command == "S":
-                    password_input3 = input("Գրեք գաղտնաբառը")
-                    if see_tasks(password_input3=password_input3):
+                    id_input3 = input("Գրեք Իդն")
+                    if see_tasks(id_input3=id_input3):
                         print("Գործողությունը կատարված է")
                     else:
                         print("-" * 25)
